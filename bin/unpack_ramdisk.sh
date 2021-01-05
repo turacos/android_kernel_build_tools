fdd3b62af09ddda06d449111993841690165d174
turacos <49122400+turacos@users.noreply.github.com>
2021-01-05 18:39:19 +0100

    Update unpack_ramdisk.sh

--- bin/unpack_ramdisk.sh
+++ bin/unpack_ramdisk.sh
@@ -4,8 +4,6 @@
  mkdir ./ramdisk
 }
 cd ramdisk
-#CMD='gzip -dc ../ramdisk.gz | cpio -idv'
-#CMD='zcat ../ramdisk.gz | cpio -idmv'
 CMD='zcat ../ramdisk.gz | cpio -idv'
 echo "---------------------------------------"
 echo "'$CMD'"
